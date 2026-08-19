8c53ccc3bd2b1ae0408c2885fde37127805124f4
D1337 <D1337@email.com>
2026-08-19 14:27:29 +0500

    Разрешил конфликт в App.jsx, добавил Services

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,16 @@
-import Header from "./components/Header"
-import Footer from "./components/Footer"
-import Prosses from "./sections/Process/Process"
-import "../src/styles/App.css"
-import "../src/components/services.css"
-import Services from "./components/Services"
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Prosses from "./sections/Process/Process";
+import "../src/styles/App.css";
+import Services from "./components/Services";
 
-
-export default function App(){
-  return(
+export default function App() {
+  return (
     <>
-      <Header/>
-<<<<<<< HEAD
-
-      <Services/>
-=======
-      <Prosses/>
-      <Footer/>
->>>>>>> 5865872888e139d3693dfae1b364528680316bc7
+      <Header />
+      <Services />
+      <Prosses />
+      <Footer />
     </>
-  )
+  );
 }
